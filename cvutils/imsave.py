340ee2dfc2f8100712fbb7a2e1b0246aa3721676
tor4z <vwenjie@hotmail.com>
2021-07-01 10:51:25 +0800

    Add data type check to imsave

--- cvutils/imsave.py
+++ cvutils/imsave.py
@@ -3,5 +3,22 @@ import cv2
 
 
 def imsave(image: np.ndarray, path: str) -> None:
+    # check data type
+    if image.dtype == np.float:
+        # check image value range
+        image_max = image.max()
+        image_min = image.min()
+        assert image_max <= 1.0 or image_min >= 0.0, \
+            f'image value (float) of out of range, image.max()={image_max},'\
+            f' image.min()={image_min}.'
+        image = (image * 255).type(np.int)
+    else:
+        # dtype of image is int
+        image_max = image.max()
+        image_min = image.min()
+        assert image_max <= 255 or image_min >= 0, \
+            f'image value (int) of out of range, image.max()={image_max},'\
+            f' image.min()={image_min}.'
+
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     cv2.imwrite(path, image)
